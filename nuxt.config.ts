02c094040b27c47b2a5b1c430b86f05ba93cfd6a
devraid <albert@axis-studios.com>
2023-03-29 10:48:31 +0200

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,24 @@ import { defineNuxtConfig } from 'nuxt/config'
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineNuxtConfig({
+	ssrLog: true,
+	components: true,
+	target: 'server',
+	head: {
+		title: 'QH',
+		htmlAttrs: {
+		  lang: 'en'
+		},
+		meta: [
+		  { charset: 'utf-8' },
+		  { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+		  { hid: 'description', name: 'description', content: '' },
+		  { name: 'format-detection', content: 'telephone=no' }
+		]
+	},
+	css: [
+		'@/scss/application.scss'
+	],
 	vite: {
 		plugins: [
 			eslintPlugin()
@@ -10,6 +28,7 @@ export default defineNuxtConfig({
 	},
 	modules: [
 		'@nuxtjs/tailwindcss',
+		'@intlify/nuxt3'
 	],
 	buildModules: [
 		['@nuxtjs/eslint-module', {
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+<script>
+export default {
+	name: 'IndexPage'
+}
+</script>
+
+<template>
+	<main class="home bg-slate-200">
+		<div class="container mx-auto p-5 bg-slate-200">
+			<h1>{{ $t('nuxt3_boilerplate') }}</h1>
+			{{ $t('description') }}
+		</div>
+	</main>
+</template>
+
+<style scoped></style>
