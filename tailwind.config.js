e54c73739d7122b06f288d5be7b9c4fb9ccd5253
devraid <albert@axis-studios.com>
2023-03-31 19:14:12 +0200

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,43 @@ module.exports = {
 	],
 	theme: {
 		container: false,
-		extend: {},
+		fontFamily: {
+			body: ['avenir-next-book', 'sans-serif'],
+			'avenir-next-book': ['avenir-next-book', 'sans-serif'],
+			'avenir-next-condensed': ['avenir-next-condensed', 'sans-serif'],
+			'avenir-next-heavy': ['avenir-next-heavy', 'sans-serif'],
+		  },
+		extend: {
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+				'2xl': '1536px',
+			  },
+			colors: {
+				green: {
+					DEFAULT: 'var(--green)',
+					7: 'var(--green-7)',
+					6: 'var(--green-6)',
+					5: 'var(--green-5)',
+					4: 'var(--green-4)',
+					3: 'var(--green-3)',
+					2: 'var(--green-2)',
+					1: 'var(--green-1)',
+				},
+				gray: {
+					DEFAULT: 'var(--gray)',
+					7: 'var(--gray-7)',
+					6: 'var(--gray-6)',
+					5: 'var(--gray-5)',
+					4: 'var(--gray-4)',
+					3: 'var(--gray-3)',
+					2: 'var(--gray-2)',
+					1: 'var(--gray-1)',
+				},
+			}
+		}
 	},
 	plugins: [],
 }
