afb3ba05254f2ec70d42a69e0fa7154acb63a16d
devraid <albert@axis-studios.com>
2023-05-10 09:53:26 +0200

    changes

--- pages/index.vue
+++ pages/index.vue
@@ -2,43 +2,49 @@
 import { useI18n } from 'vue-i18n'
 
 export default {
-	name: 'IndexPage',
-	components: {},
-	setup() {
-		const { t } = useI18n()
-		return { t}
-	},
-	data: function() {
-		return {
-			visibleSignRegister: true
-		}
-	},
-	mounted () {
-		console.log('hello world');
-		this.$nextTick(function () {
-			let everythingLoaded = setInterval(function (scope) {
-				if (/loaded|complete/.test(document.readyState)) {
-					clearInterval(everythingLoaded)
-					console.log('Fonts and images are loaded, let\'s start the show!')
-					scope.init()
-				}
-			}, 10, this)
-		})
-	},
-	methods: {
-		init () {
-			console.log('Fully loaded.')
-		}
-	}
+  name: 'IndexPage',
+  components: {},
+  setup() {
+    const { t } = useI18n()
+    return { t }
+  },
+  data: function () {
+    return {
+      visibleSignRegister: true,
+    }
+  },
+  mounted() {
+    this.$nextTick(function () {
+      let everythingLoaded = setInterval(
+        function (scope) {
+          if (/loaded|complete/.test(document.readyState)) {
+            clearInterval(everythingLoaded)
+            console.log("Fonts and images are loaded, let's start the show!")
+            scope.init()
+          }
+        },
+        10,
+        this
+      )
+    })
+  },
+  methods: {
+    init() {
+      console.log('Fully loaded.')
+    },
+  },
 }
 </script>
 
 <template>
-	<div class="home">
-		<FormsSignInRegister
-			v-if="visibleSignRegister"
-			@close="() => {
-				visibleSignRegister = false
-			}" />
-	</div>
+  <div class="home">
+    <FormsSignInRegister
+      v-if="visibleSignRegister"
+      @close="
+        () => {
+          visibleSignRegister = false
+        }
+      "
+    />
+  </div>
 </template>
