479fe9ab38742b83713008a12b87e72d88b1ca6b
devraid <albert@axis-studios.com>
2023-05-13 21:35:25 +0200

    changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
 		'nuxt/no-cjs-in-config': 'off',
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'vue/html-indent': ['error', 'tab'],
 		'vue/html-closing-bracket-newline': 'off',
 		indent: ['error', 'tab'],
 		'no-tabs': 'off',
